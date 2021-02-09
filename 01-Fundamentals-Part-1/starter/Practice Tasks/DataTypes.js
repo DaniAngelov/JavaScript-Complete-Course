740845943229d3947f1c67995acdbf1576361668
Dani <djena@abv.bg>
2021-02-09 10:44:24 +0200

    Added first coding Challenges and Practice Exercises from Section 1

---
+++ 01-Fundamentals-Part-1/starter/Practice Tasks/DataTypes.js
@@ -0,0 +1,10 @@
+//2. Data Types
+
+let country = "Bulgaria"
+let isIsland = false;
+let language;
+let population = 7000000;
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
