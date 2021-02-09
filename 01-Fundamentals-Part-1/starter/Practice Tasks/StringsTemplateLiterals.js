ec6189be529239b238d3cc3aeed00e5c4cab1e61
DaniAngelov <60110955+DaniAngelov@users.noreply.github.com>
2021-02-09 10:47:32 +0200

    Delete 01-Fundamentals-Part-1/starter directory

--- 01-Fundamentals-Part-1/starter/Practice Tasks/StringsTemplateLiterals.js
+++
@@ -1,6 +0,0 @@
-// 5. Strings and Template Literals
-
-const description = `Portugal is in Europe, and its 11 million
-people speak portuguese`;
-
-console.log(description);
--- 01-Fundamentals-Part-1/starter/Practice Tasks/TernaryOperator.js
+++
@@ -1,7 +0,0 @@
-//11. The Conditional (Ternary) Operator
-
-
-const population = 7000000;
-
-const bulgariaPop = population > 30000000 ? "Buglaria's population is above average" : "Buglaria's population is below average";
-console.log(bulgariaPop);
